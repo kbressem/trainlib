a9619160fba3dc565e6ef4c9f95520160e525ff5
kbressem <kenobressem@gmail.com>
2022-06-22 13:54:42 +0200

    Add possibility to add valid-only transforms fix bug in how tfms are called

--- trainlib/transforms.py
+++ trainlib/transforms.py
@@ -14,16 +14,23 @@ def get_transform(tfm_name: str, config: dict, **kwargs):
     except AttributeError:
         if hasattr(monai.transforms, tfm_name):
             transform = getattr(monai.transforms, tfm_name)
-            assert "dictionary" in transform.__module__, f"{tfm_name} is not a dictionary transform"
+            assert (
+                "dictionary" in transform.__module__
+            ), f"{tfm_name} is not a dictionary transform"
         else:
             raise AttributeError(
                 f"{tfm_name} not in `monai.transforms` nor in {config.patch.transforms}"
             )
-    transform_configs = {**config.transforms.base, **config.transforms.train}
-    if tfm_name in transform_configs.keys():
-        for k in kwargs.keys():
-            transform_configs[tfm_name].pop(k)
-        kwargs = {**transform_configs[tfm_name], **kwargs}
+    # merge config for transform type (train, valid, test) with kwargs
+    for k in config.transforms.keys():
+        if (
+            isinstance(config.transforms[k], dict)
+            and tfm_name in config.transforms[k].keys()
+        ):
+            transform_config = config.transforms[k]
+            for k in kwargs.keys():
+                transform_config[tfm_name].pop(k)
+            kwargs = {**transform_config[tfm_name], **kwargs}
 
     allowed_kwargs = transform.__init__.__code__.co_varnames
     if "keys" not in kwargs.keys():
@@ -41,8 +48,9 @@ def get_base_transforms(config: dict) -> List[Callable]:
         get_transform("LoadImaged", config=config, allow_missing_keys=True),
         get_transform("EnsureChannelFirstd", config=config, allow_missing_keys=True),
     ]
-    tfm_names = [tn for tn in config.transforms.base]
-    tfms += [get_transform(tn, config) for tn in tfm_names]
+    if "base" in config.transforms.keys():
+        tfm_names = [tn for tn in config.transforms.base]
+        tfms += [get_transform(tn, config) for tn in tfm_names]
     return tfms
 
 
@@ -57,10 +65,10 @@ def get_train_transforms(config: dict) -> Compose:
 
     # some arguments in config.transforms are not a transform but a global argument
     # such as the probability a transfor is applied
-
-    tfm_names = [tn for tn in config.transforms.train]
-    train_tfms = [get_transform(tn, config) for tn in tfm_names]
-    tfms += [tfm for tfm in train_tfms if tfm not in tfms]  # add rest
+    if "train" in config.transforms.keys():
+        tfm_names = [tn for tn in config.transforms.train]
+        train_tfms = [get_transform(tn, config) for tn in tfm_names]
+        tfms += [tfm for tfm in train_tfms if tfm not in tfms]  # add rest
 
     # Concat mutlisequence data to single Tensors on the ChannelDim
     # Rename images to `CommonKeys.IMAGE` and labels to `CommonKeys.LABELS`
@@ -82,15 +90,23 @@ def get_train_transforms(config: dict) -> Compose:
             allow_missing_keys=True,
         ),
         get_transform(
-            "ConcatItemsd", config=config, keys=config.data.image_cols, name=CommonKeys.IMAGE, dim=0
+            "ConcatItemsd",
+            config=config,
+            keys=config.data.image_cols,
+            name=CommonKeys.IMAGE,
+            dim=0,
         ),
         get_transform(
-            "ConcatItemsd", config=config, keys=config.data.label_cols, name=CommonKeys.LABEL, dim=0
+            "ConcatItemsd",
+            config=config,
+            keys=config.data.label_cols,
+            name=CommonKeys.LABEL,
+            dim=0,
         ),
     ]
     if config.debug:
         tfms.append(get_transform("DataStatsd", config=config, allow_missing_keys=True))
-    
+
     return Compose(tfms)
 
 
@@ -113,17 +129,32 @@ def get_val_transforms(config: dict) -> Compose:
             keys=config.data.image_cols,
             allow_missing_keys=True,
         ),
-        get_transform(
-            "ConcatItemsd", config=config, keys=config.data.image_cols, name=CommonKeys.IMAGE, dim=0
-        ),
-        get_transform(
-            "ConcatItemsd", config=config, keys=config.data.label_cols, name=CommonKeys.LABEL, dim=0
-        ),
     ]
+    if "valid" in config.transforms.keys():
+        tfm_names = [tn for tn in config.transforms.valid]
+        tfms += [get_transform(tn, config) for tn in tfm_names]
+
     if config.debug:
         tfms.append(get_transform("DataStatsd", config=config, allow_missing_keys=True))
 
+    tfms += [
+        get_transform(
+            "ConcatItemsd",
+            config=config,
+            keys=config.data.image_cols,
+            name=CommonKeys.IMAGE,
+            dim=0,
+        ),
+        get_transform(
+            "ConcatItemsd",
+            config=config,
+            keys=config.data.label_cols,
+            name=CommonKeys.LABEL,
+            dim=0,
+        ),
+    ]
     return Compose(tfms)
+
 
 def get_test_transforms(config: dict) -> Compose:
     "Transforms applied only to the test dataset"
@@ -145,10 +176,19 @@ def get_test_transforms(config: dict) -> Compose:
             allow_missing_keys=True,
         ),
         get_transform(
-            "ConcatItemsd", config=config, keys=config.data.image_cols, name=CommonKeys.IMAGE, dim=0
+            "ConcatItemsd",
+            config=config,
+            keys=config.data.image_cols,
+            name=CommonKeys.IMAGE,
+            dim=0,
         ),
         get_transform(
-            "ConcatItemsd", config=config, keys=config.data.label_cols, name=CommonKeys.LABEL, dim=0, allow_missing_keys=True
+            "ConcatItemsd",
+            config=config,
+            keys=config.data.label_cols,
+            name=CommonKeys.LABEL,
+            dim=0,
+            allow_missing_keys=True,
         ),
     ]
 
@@ -158,7 +198,9 @@ def get_test_transforms(config: dict) -> Compose:
 def get_val_post_transforms(config: dict):
     "Transforms applied to the model output, before metrics are calculated"
     tfms = [
-        get_transform("EnsureTyped", config=config, keys=[CommonKeys.PRED, CommonKeys.LABEL]),
+        get_transform(
+            "EnsureTyped", config=config, keys=[CommonKeys.PRED, CommonKeys.LABEL]
+        ),
         get_transform(
             "AsDiscreted",
             config=config,
