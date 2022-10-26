bfaae69d85818b3ccd250994533bb117cc6f5863
Markus Hinsche <m.hinsche@gmail.com>
2022-10-26 09:38:56 +0200

    Rename loss_fun to loss_class
    e.g. https://pytorch.org/docs/master/generated/torch.nn.MSELoss.html#torch.nn.MSELoss

--- trainlib/loss.py
+++ trainlib/loss.py
@@ -22,11 +22,11 @@ def get_loss(config: munch.Munch) -> Callable:
     loss_type = list(config.loss.keys())[0]
     loss_config = config.loss[loss_type]
     if hasattr(monai_losses, loss_type):
-        loss_fun = getattr(monai_losses, loss_type)
+        loss_class = getattr(monai_losses, loss_type)
     elif hasattr(torch.nn, loss_type):
-        loss_fun = getattr(torch.nn, loss_type)
+        loss_class = getattr(torch.nn, loss_type)
     else:
         raise AttributeError(f"Loss function '{loss_type}' is neither in `monai.losses` nor in `torch.nn`")
 
-    loss = loss_fun(**loss_config)
+    loss = loss_class(**loss_config)
     return loss
