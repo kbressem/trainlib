b4895e77860b993a241745e363570bcc8056614b
kbressem <kenobressem@gmail.com>
2022-11-18 11:37:59 +0100

    Update data.py

--- trainlib/data.py
+++ trainlib/data.py
@@ -112,8 +112,8 @@ class DataLoader(MonaiDataLoader):
 
         ListViewer(
             [image_transform(im) for im in image_list],
-            [label_transform(im) for im in label_list],
-            mode=mode,  # type: ignore
+            [label_transform(im) for im in label_list],  # type: ignore
+            mode=mode,  
             **kwargs,
         ).show()
 
